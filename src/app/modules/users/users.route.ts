a43d38d8025f0d2ac3d374fb3741be5dc2156626
rajibdas-it <rajibrad@gmail.com>
2023-05-31 17:11:28 +0600

    [done] UN-6 user controller and service done.

---
+++ src/app/modules/users/users.route.ts
@@ -0,0 +1,6 @@
+import express from 'express'
+import { createUserController } from './users.controller'
+const router = express.Router()
+
+router.post('/create-user', createUserController)
+export default router
