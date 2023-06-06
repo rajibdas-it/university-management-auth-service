9f5c15954b70047513966a63627c556ec9f98cda
rajibdas-it <rajibrad@gmail.com>
2023-06-07 00:59:16 +0600

    beautify winston

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,25 @@
-import winston from 'winston'
+import { createLogger, format, transports } from 'winston'
 import path from 'path'
+const { combine, timestamp, label, printf, prettyPrint } = format
 
-export const logger = winston.createLogger({
+//custom log format
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const currentDate = date.toDateString()
+  const hour = date.getHours()
+  const mins = date.getMinutes()
+  const secs = date.getSeconds()
+
+  return `${currentDate} ${hour}:${mins}:${secs} [${label}] ${level}: ${message}`
+})
+
+export const logger = createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat, prettyPrint()),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
+    new transports.Console(),
+    new transports.File({
       // eslint-disable-next-line no-undef
       filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'),
       level: 'info',
@@ -15,14 +27,13 @@ export const logger = winston.createLogger({
   ],
 })
 
-export const errorlogger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
+export const errorlogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'PH' }), timestamp(), myFormat, prettyPrint()),
   transports: [
-    new winston.transports.Console(),
+    new transports.Console(),
 
-    new winston.transports.File({
+    new transports.File({
       // eslint-disable-next-line no-undef
       filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'),
       level: 'error',
